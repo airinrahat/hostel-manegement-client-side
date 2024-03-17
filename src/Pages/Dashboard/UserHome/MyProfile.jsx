82cb30ab212200fbe83be8683b5225f89414985b
Airin Rahat Prome <airinrahatprome@gmail.com>
2024-03-17 12:44:12 +0600

    update admin

--- src/Pages/Dashboard/UserHome/MyProfile.jsx
+++ src/Pages/Dashboard/UserHome/MyProfile.jsx
@@ -31,7 +31,7 @@ const MyProfile = () => {
         Welcome Back!
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+      {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
         <div className="flex justify-center items-center space-x-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-8 py-8 rounded-lg">
           <span className="text-5xl">
             <SiTask></SiTask>
@@ -61,7 +61,7 @@ const MyProfile = () => {
             <p className="text-lg font-bold">Reviews</p>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <div className="lg:flex px-2 lg:flex-col lg:justify-center lg:items-center shadow-xl rounded-2xl">
         <div className="h-24 lg:w-2/3 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
